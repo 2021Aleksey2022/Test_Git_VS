e6329eeb917f88d7333184c1750b04ad5809f0d6
Алескей Щербаков <manay.ru@mail.ru>
2023-12-01 21:32:00 +1000

    Моя первая программа

---
+++ Example001_helloWorld/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_1.Example001_helloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
